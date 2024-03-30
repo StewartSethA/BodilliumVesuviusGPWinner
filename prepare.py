274b43c67268a57ae04caf68b04ddfd8b11e73a6
Bodillium <nicola.bodill@gmail.com>
2024-03-30 22:11:05 +1100

    Update prepare.py for use with Colab

--- prepare.py
+++ prepare.py
@@ -27,11 +27,11 @@ def read_image_mask(fragment_id,start_idx=15,end_idx=45):
     if fragment_id in ['20230702185753','20230929220926','20231005123336','20231007101619','20231012184423','20231016151002','20231022170901','20231031143852','20231106155351','20231210121321','20231221180251','20230820203112']:
         images=images[:,:,::-1]
     if fragment_id in ['20231022170901','20231022170900']:
-        mask = cv2.imread( f"train_scrolls/{fragment_id}/{fragment_id}_inklabels.tiff", 0)
+        mask = cv2.imread( f"/content/gdrive/MyDrive/modelmain/training/train_scrolls/{fragment_id}/{fragment_id}_inklabels.tiff", 0)
     else:
-        mask = cv2.imread(f"train_scrolls/{fragment_id}/{fragment_id}_inklabels.png", 0)
+        mask = cv2.imread(f"/content/gdrive/MyDrive/modelmain/training/train_scrolls/{fragment_id}/{fragment_id}_inklabels.png", 0)
 
-    fragment_mask=cv2.imread( f"train_scrolls/{fragment_id}/{fragment_id}_mask.png", 0)
+    fragment_mask=cv2.imread( f"/content/gdrive/MyDrive/modelmain/training/train_scrolls/{fragment_id}/{fragment_id}_mask.png", 0)
     fragment_mask = np.pad(fragment_mask, [(0, pad0), (0, pad1)], constant_values=0)
     mask = mask.astype('float32')
     mask/=255
@@ -40,22 +40,22 @@ def read_image_mask(fragment_id,start_idx=15,end_idx=45):
 def run_sanity_checks():
     for fragment_id in ['20230702185753','20230929220926','20231005123336','20231007101619','20231012184423','20231016151002','20231022170901','20231031143852','20231106155351','20231210121321','20231221180251','20230820203112']:  
         print(fragment_id)
-        assert os.path.exists(f'train_scrolls/{fragment_id}/layers/00.tif')
-        assert os.path.exists(f'train_scrolls/{fragment_id}/{fragment_id}_inklabels.png')
-        assert os.path.exists(f'train_scrolls/{fragment_id}/{fragment_id}_mask.png')
-    assert os.path.exists(f'train_scrolls/20231022170901/layers/00.tif')
-    assert os.path.exists(f'train_scrolls/20231022170901/20231022170901_inklabels.tiff')
-    assert os.path.exists(f'train_scrolls/20231022170901/20231022170901_mask.png')
+        assert os.path.exists(f'/content/gdrive/MyDrive/modelmain/training/train_scrolls/{fragment_id}/layers/00.tif')
+        assert os.path.exists(f'/content/gdrive/MyDrive/modelmain/training/train_scrolls/{fragment_id}/{fragment_id}_inklabels.png')
+        assert os.path.exists(f'/content/gdrive/MyDrive/modelmain/training/train_scrolls/{fragment_id}/{fragment_id}_mask.png')
+    assert os.path.exists(f'/content/gdrive/MyDrive/modelmain/training/train_scrolls/20231022170901/layers/00.tif')
+    assert os.path.exists(f'/content/gdrive/MyDrive/modelmain/training/train_scrolls/20231022170901/20231022170901_inklabels.tiff')
+    assert os.path.exists(f'/content/gdrive/MyDrive/modelmain/training/train_scrolls/20231022170901_mask.png')
 def prepare_data():
     for l in os.listdir('all_labels/'):
         if '.png' in l:
             f_id=l[:-14]
-            if os.path.exists(f'train_scrolls/{f_id}'):
+            if os.path.exists(f'/content/gdrive/MyDrive/modelmain/training/train_scrolls/{f_id}'):
                 img=cv2.imread(f'all_labels/{f_id}_inklabels.png', 0)
-                cv2.imwrite(f"train_scrolls/{f_id}/{f_id}_inklabels.png", img) 
+                cv2.imwrite(f"/content/gdrive/MyDrive/modelmain/training/train_scrolls/{f_id}/{f_id}_inklabels.png", img) 
         if '.tiff' in l:
             f_id=l[:-15]
-            if os.path.exists(f'train_scrolls/{f_id}'):
+            if os.path.exists(f'/content/gdrive/MyDrive/modelmain/training/train_scrolls/{f_id}'):
                 img=cv2.imread(f'all_labels/{f_id}_inklabels.tiff', 0)
                 cv2.imwrite(f"train_scrolls/{f_id}/{f_id}_inklabels.tiff", img) 
 if __name__ == "__main__":
